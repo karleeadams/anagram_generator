3c817a111fa07d387c50a5366cbe64d049f98afb
Karlee Adams <gunnell.karlee@gmail.com>
2015-11-04 15:59:32 -0700

    Adding words controller

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,5 @@
 def three_letters?(input)
-	if input.length >= 3
+	if input.length > 3
 		true
 	else
 		false
@@ -29,7 +29,8 @@ end
 
 get '/anagrams/:word' do
 	@word = params[:word]
-	@anagrams = Word.find_anagrams(@word)
+	alphabetized_string = @word.chars.sort.join
+	@anagrams = Word.where("letters=?", alphabetized_string)
 	erb :show
 end
 
