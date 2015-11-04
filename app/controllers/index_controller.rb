63bdec363ab6b7e19c653cd090d48d737091b3b7
Karlee Adams <gunnell.karlee@gmail.com>
2015-11-04 13:41:11 -0700

    Handling Constraints

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,3 +1,28 @@
+def three_letters?(input)
+	if input.length >= 3
+		true
+	else
+		false
+	end
+end
+
+def distinct_letters?(input)
+   letter_array = input.chars
+   unique_letters = letter_array.uniq
+   if unique_letters.length < letter_array.length
+     false
+   else
+     true
+   end
+end
+
+def valid_input(input)
+  if input.length > 3
+     raise Exception.new("Word must be less than or equal to 3 characters.")
+  end
+end
+
+
 get '/' do
   erb :index
 end
@@ -9,7 +34,13 @@ get '/anagrams/:word' do
 end
 
 post '/' do
-	@word = params[:word]
-	redirect "/anagrams/#{@word}"
+    word = params[:word]
+    begin 
+      valid_input(word)
+      redirect "/anagrams/#{word}"
+    rescue Exception => error
+		@error = "Oops! Something must be wrong. Please make sure you spelled the word correctly."
+		erb :index
+	end
 end
 
