6b32fc7c7ea6c104b00ada1eca514615cd9bf2fb
Karlee Adams <gunnell.karlee@gmail.com>
2015-11-05 14:15:32 -0700

    Adding pages

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,14 +1,40 @@
 get '/words' do
-	@words = Word.all
-	erb :"/words/index"
+  @words = Word.all
+  erb :"/words/index"
 end
 
-post'/words' do
-	word = Word.create(text: params[:text])
-	redirect "/words/#{word.id}"
+post '/words' do
+  word = Word.create(text: params[:text])
+  redirect "/words/#{word.id}"
 end
 
 get '/words/new' do
   @word = Word.new
   erb :"/words/new"
+end
+
+get '/words/:id/edit' do
+  @word = Word.find_by_id(params[:id])
+
+  erb :"/words/edit"
+end
+
+delete '/words/:id' do
+  word = Word.find_by_id(params[:id])
+  word.destroy
+
+  redirect "/words"
+end
+
+put '/words/:id' do
+  @word = Word.find_by_id(params[:id])
+  @word.text = "#{params[:text]}"
+  @word.save
+
+  erb :"/words/show"
+end
+
+get '/words/:id' do
+  @word = Word.find_by(id: params[:id])
+  erb :"/words/show"
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,31 +1,32 @@
 module ApplicationHelper
-	def words_form_action(word)
-    	if word.new_record?
-      		@action = "/words"
-   	 	else
-    		@action = "/words/#{word.id}"
-    	end
+  def words_form_action(word)
+    if word.new_record?
+      @action = "/words"
+    else
+      @action = "/words/#{word.id}"
+    end
     @action
-end
+  end
 
-	 def words_form_method(word)
-	 	if word.new_record?
-	 		@method = "post"
-	 	else
-	 		@method = "put"
-	 	end
-	 end
+  def words_form_method(word)
+    if word.new_record?
+      @method = "post"
+    else
+      @method = "put"
+    end
+  end
 
-	def words_form_class(word)
-		@class
-	end
+  def words_form_class(word)
 
-	def words_form_id(word)
-		if word.new_record?
-			@form_id = "new-word"
-		else
-			@form_id = "edit-word"
-		end
-		@form_id
-	end
+    @class
+  end
+
+  def words_form_id(word)
+    if word.new_record?
+      @form_id = "new-word"
+    else
+      @form_id = "edit-word"
+    end
+    @form_id
+  end
 end
